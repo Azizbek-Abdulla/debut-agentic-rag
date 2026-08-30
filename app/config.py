133a3bbcaa298c88c1e0f9156fc72c8758c98373
Azizbek Abdulla <azizbekpythonlearning@gmail.com>
2026-08-30 16:23:39 +0500

    fix(app/config): fixed the wrongly written qdrant_api_key's default so that it takes the value of respective data type

--- app/config.py
+++ app/config.py
@@ -17,7 +17,7 @@ class Settings(BaseSettings):
     llm_temperature: float = Field(default = 0.0)
 
     qdrant_url: str = Field(default = "http://localhost:6333")
-    qdrant_api_key: None = Field(default = None)
+    qdrant_api_key: str | None = Field(default = None)
     qdrant_collection: str = Field(default="rag_documents")
 
     embedding_model: str = Field(default="BAAI/bge-m3")
