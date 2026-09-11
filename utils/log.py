6ab4a4ae899812e02bc63513e41ebe3521c04380
Azizbek Abdulla <azizbekpythonlearning@gmail.com>
2026-09-11 10:57:42 +0500

    fix(utils/log): added message to the console sink so that the traceback becomes visible to the user

--- utils/log.py
+++ utils/log.py
@@ -47,7 +47,7 @@ def configure_logging(log_dir: str = "logs", level: str = "INFO") -> None:
         format=(
             "<green>{time:YYYY-MM-DD HH:mm:ss}</green> | "
             "<level>{level: <8}</level> | "
-            "<cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> "
+            "<cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> - <level>{message}</level>"
         ),
         colorize=True, 
         backtrace=False, 
